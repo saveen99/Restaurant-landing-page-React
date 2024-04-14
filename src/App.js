63d2ab6331d2404edb7bb9930244784d34370b6e
saveen-maduranga <saveenonline@gmail.com>
2024-04-14 13:35:24 +0530

    Update Files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Home from './Components/Home';
 import About from './Components/About';
+import Work from './Components/Work';
+import Testimonial from './Components/Testimonial';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 
 function App() {
   return <div className="App">
     <Home/>
     <About/>
+    <Work/>
+    <Testimonial/>
+    <Contact/>
+    <Footer/>
   </div>;
 }
 
