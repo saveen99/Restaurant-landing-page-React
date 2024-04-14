d20149aacfa537c0aa2a7d488808e3913df15a4b
saveen-maduranga <saveenonline@gmail.com>
2024-04-14 13:00:14 +0530

    Update Files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Home from './Components/Home';
+import About from './Components/About';
 
 function App() {
   return <div className="App">
     <Home/>
+    <About/>
   </div>;
 }
 
